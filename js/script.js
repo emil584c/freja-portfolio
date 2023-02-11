96a5abedf497222a2663492cbf1dfd9023f5ebfb
emil584c <105641442+emil584c@users.noreply.github.com>
2023-02-11 18:03:02 +0100

    alt

--- js/script.js
+++ js/script.js
@@ -15,4 +15,20 @@ function toggleHamburger(e) {
   bars.forEach(bar => bar.classList.toggle('x'))
 }
 
-navToggle.addEventListener('click', toggleHamburger)
+navToggle.addEventListener('click', toggleHamburger);
+
+
+
+const projectButton = document.getElementById('projects');
+const projects = document.getElementById('project-list')
+
+projectButton.addEventListener('click', function handleClick() {
+    if (projects.style.display = "none"){
+        projects.style.display = "flex";
+    }else {
+        projects.display ="none";
+    }
+})
+
+
+
